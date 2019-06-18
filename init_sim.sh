46b4e9f8fa4d2eed0504eb419b96e2792ce053e4
herredro <donlukon@gmail.com>
2019-06-18 11:01:20 +0200

    hoho fix

---
+++ init_sim.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+sudo apt-get install git
+git clone https://github.com/herredro/eShipSimulation.git
+cd eShipSimulation/
+sudo apt install python3-pip
+pip3 install simpy
+pip3 install numpy
+pip3 install colorama
+pip3 install tabulate
+pip3 install matplotlib
